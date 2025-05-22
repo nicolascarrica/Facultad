d4b4ce7dc3c4509c26ecef1a7319ea86eeee1457
Nicolas Carrica <nicolas.carrica@gmail.com>
2025-05-21 23:16:20 -0300

    comenta lineas

--- Semestre-3/RUST/Practica/tps/src/tp03/mod.rs
+++ Semestre-3/RUST/Practica/tps/src/tp03/mod.rs
@@ -3,8 +3,9 @@
 // pub mod ej03;
 // pub mod ej04;
 // pub mod ej05;
-pub mod ej06;
-//pub mod ej07;
+// pub mod ej06;
+// pub mod nicolas_carrica_v2;
+// pub mod ej07;
 // pub mod ej07v2;
 // //pub mod ej08;
 // pub mod ej08v2;
