ce405984490939f1469a9bf687ced01c73887fba
Nicolas Carrica <nicolas.carrica@gmail.com>
2025-06-19 12:29:45 -0300

    Add mod tp4

--- Semestre-3/RUST/Practica/tps/src/tp04/mod.rs
+++ Semestre-3/RUST/Practica/tps/src/tp04/mod.rs
@@ -1,7 +1,8 @@
 // pub mod ej01;
-// pub mod ej02;
-// pub mod ej03;
-// pub mod ej04;
-pub mod ej04_v2;
+//pub mod ej02;
+//pub mod ej03;
+//pub mod ej04;
+//pub mod ej04_v2;
 //pub mod ej05;
-pub mod ej05v1;
+//pub mod ej05v1;
+//pub mod ej05_v3;
