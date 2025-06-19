38fbb40bb2ae09e9ab6eface53b811d2a9335d2e
Nicolas Carrica <nicolas.carrica@gmail.com>
2025-06-19 12:28:50 -0300

    implement partial eq for MedioDePago

--- Semestre-3/RUST/Practica/tps/src/tp04/ej03.rs
+++ Semestre-3/RUST/Practica/tps/src/tp04/ej03.rs
@@ -24,13 +24,20 @@
 use std::collections::HashMap;
 
 use crate::tp03::ej03::Fecha;
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+use std::mem::discriminant;
+#[derive(Debug, Clone, Eq, Hash)]
 enum MedioDePago {
     Efectivo,
     MercadoPago {cvu: String},
     TarjetaDeCredito {numero: String, cvv: String},
     TransferenciaBancaria {cbu: String},
     Cripto {direccion: String},
+}
+
+impl PartialEq for MedioDePago {
+    fn eq(&self, other: &Self) -> bool {
+        discriminant(self) == discriminant(other)
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -61,33 +68,6 @@ impl Usuario { // preferi implementar esto en un struc de usuario, luego desde l
             pago,
         }
     }
-
-    fn upgrade(&mut self) {
-        if let Some(ref mut susc) = self.suscripcion {
-            susc.tipo = match susc.tipo {
-                TipoSuscripcion::Basic => TipoSuscripcion::Clasic,
-                TipoSuscripcion::Clasic => TipoSuscripcion::Super,
-                TipoSuscripcion::Super => TipoSuscripcion::Super,
-            };
-        }
-    }
-
-    fn downgrade(&mut self) {
-        if let Some(ref mut susc) = self.suscripcion {
-            susc.tipo = match susc.tipo {
-                TipoSuscripcion::Super => TipoSuscripcion::Clasic,
-                TipoSuscripcion::Clasic => TipoSuscripcion::Basic,
-                TipoSuscripcion::Basic => {
-                    self.suscripcion = None;
-                    return;
-                },
-            };
-        }    
-    }
-
-    fn cancelar_suscripcion(&mut self) {
-        self.suscripcion = None;
-    }
 }
 
 struct Plataforma {
@@ -101,6 +81,71 @@ impl Plataforma {
             usuarios: Vec::new(),
         }
     }
+
+    fn agregar_usuario(&mut self, id: u32, suscripcion: Option<Suscripcion>, pago: MedioDePago) -> Result<(), String> {
+        if self.usuarios.iter().any(|u| u.id == id) {
+            Err(format!("Ya existe un usuario con el ID {}", id))
+        } else {
+            self.usuarios.push(Usuario::new(id, suscripcion, pago));
+            Ok(())
+        }
+    }
+
+    fn obtener_usuario(&mut self, id: u32) -> Option<&mut Usuario> {
+        self.usuarios.iter_mut().find(|u| u.id == id)
+    }
+
+    // Métodos para manejar suscripciones desde la plataforma
+    fn upgrade_usuario(&mut self, id: u32) -> Result<(), String> {
+        let usuario = self.obtener_usuario(id).ok_or("Usuario no encontrado")?;
+
+        if let Some(ref mut susc) = usuario.suscripcion {
+            match susc.tipo {
+                TipoSuscripcion::Basic => {
+                    susc.tipo = TipoSuscripcion::Clasic;
+                    Ok(())
+                },
+                TipoSuscripcion::Clasic => {
+                    susc.tipo = TipoSuscripcion::Super;
+                    Ok(())
+                },
+                TipoSuscripcion::Super => Err(format!("El usuario {} ya tiene la suscripción Super (máxima)", id)),
+            }
+        } else {
+            Err(format!("El usuario {} no tiene suscripción activa para hacer upgrade", id))
+        }
+    }   
+
+    fn downgrade_usuario(&mut self, id: u32) -> Result<(), String> {
+        let usuario = self.obtener_usuario(id).ok_or("Usuario no encontrado")?;
+        
+        if let Some(ref mut susc) = usuario.suscripcion {
+            susc.tipo = match susc.tipo {
+                TipoSuscripcion::Super => TipoSuscripcion::Clasic,
+                TipoSuscripcion::Clasic => TipoSuscripcion::Basic,
+                TipoSuscripcion::Basic => {
+                    usuario.suscripcion = None;
+                    return Ok(());
+                },
+            };
+            Ok(())
+        } else {
+            Err(format!("El usuario {} no tiene suscripción activa para hacer downgrade", id))
+        }
+    }
+
+    fn cancelar_suscripcion_usuario(&mut self, id: u32) -> Result<(), String> {
+        let usuario = self.obtener_usuario(id).ok_or("Usuario no encontrado")?;
+        
+        if usuario.suscripcion.is_some() {
+            usuario.suscripcion = None;
+            Ok(())
+        } else {
+            Err(format!("El usuario {} no tiene suscripción activa para cancelar", id))
+        }
+    }
+
+
 // el uso de hashmap es para poder contar la cantidad de veces que aparece un elemento en un vector.
     fn medio_pago_mas_usado_activo(&self) -> Option<MedioDePago> {
         let mut contador = HashMap::new();
@@ -172,86 +217,128 @@ mod test {
         }
     }
 
-    fn crear_usuario(id: u32, suscripcion: Option<Suscripcion>, pago: MedioDePago) -> Usuario {
-        Usuario::new(id, suscripcion, pago)
-    }
-
     #[test]
     fn test_upgrade() {
-        let mut usuario = crear_usuario(1, Some(suscripcion_basic()), MedioDePago::Efectivo);
-        usuario.upgrade();
+        let mut plataforma = Plataforma::new();
+        plataforma.agregar_usuario(1, Some(suscripcion_basic()), MedioDePago::Efectivo);
         
-        if let Some(sub) = &usuario.suscripcion {
-            assert_eq!(sub.tipo, TipoSuscripcion::Clasic);
+        // 1° act Basic → Clasic
+        assert!(plataforma.upgrade_usuario(1).is_ok());
+
+        if let Some(usuario) = plataforma.obtener_usuario(1) {
+            if let Some(sub) = &usuario.suscripcion {
+                assert_eq!(sub.tipo, TipoSuscripcion::Clasic);
+            } else {
+                panic!("Se esperaba una suscripción Clasic");
+            }
+        } else {
+            panic!("Usuario no encontrado");
         }
 
-        usuario.upgrade();
-        
-        if let Some(sub) = &usuario.suscripcion {
-            assert_eq!(sub.tipo, TipoSuscripcion::Super);
+        // 2° act Clasic → Super
+        assert!(plataforma.upgrade_usuario(1).is_ok());
+
+        if let Some(usuario) = plataforma.obtener_usuario(1) {
+            if let Some(sub) = &usuario.suscripcion {
+             assert_eq!(sub.tipo, TipoSuscripcion::Super);
+            } else {
+                panic!("Se esperaba una suscripción Super");
+            }
+        } else {
+            panic!("Usuario no encontrado");
         }
 
-        usuario.upgrade();
-        
-        if let Some(sub) = &usuario.suscripcion {
-            assert_eq!(sub.tipo, TipoSuscripcion::Super);
-        }
+        // 3° act Super → Super
+        assert!(plataforma.upgrade_usuario(1).is_err());
+
     }
 
     #[test]
     fn test_downgrade() {
-        let mut usuario = crear_usuario(1, Some(suscripcion_super()), MedioDePago::Efectivo);
-        usuario.downgrade();
-        
-        if let Some(sub) = &usuario.suscripcion {
-            assert_eq!(sub.tipo, TipoSuscripcion::Clasic);
+        let mut plataforma = Plataforma::new();
+        plataforma.agregar_usuario(1, Some(suscripcion_super()), MedioDePago::Efectivo);
+
+        // 1° act Super → clasic
+        assert!(plataforma.downgrade_usuario(1).is_ok());
+
+        if let Some(usuario) = plataforma.obtener_usuario(1) {
+            if let Some(sub) = &usuario.suscripcion {
+                assert_eq!(sub.tipo, TipoSuscripcion::Clasic);
+            } else {
+                panic!("Se esperaba una suscripción Clasic");
+            }
+        } else {
+            panic!("Usuario no encontrado");
         }
 
-        usuario.downgrade();
-        
-        if let Some(sub) = &usuario.suscripcion {
-            assert_eq!(sub.tipo, TipoSuscripcion::Basic);
+    // 2° downgrade: clasic → absic
+        assert!(plataforma.downgrade_usuario(1).is_ok());
+
+        if let Some(usuario) = plataforma.obtener_usuario(1) {
+            if let Some(sub) = &usuario.suscripcion {
+             assert_eq!(sub.tipo, TipoSuscripcion::Basic);
+            } else {
+                panic!("Se esperaba una suscripción Basic");
+            }
+        } else {
+            panic!("Usuario no encontrado");
         }
 
-        usuario.downgrade();
-        
+    // 3° downgrade: Basic a nada
+        assert!(plataforma.downgrade_usuario(1).is_ok());
+
+        if let Some(usuario) = plataforma.obtener_usuario(1) {
         assert!(usuario.suscripcion.is_none());
+        } else {
+            panic!("Usuario no encontrado");
+        }
     }
 
     #[test]
     fn test_cancelar_suscripcion() {
-        let mut usuario = crear_usuario(1, Some(suscripcion_super()), MedioDePago::Efectivo);
-        usuario.cancelar_suscripcion();
-        
-        assert!(usuario.suscripcion.is_none());
+        let mut plataforma = Plataforma::new();
+        plataforma.agregar_usuario(1, Some(suscripcion_super()), MedioDePago::Efectivo);
+
+        assert!(plataforma.cancelar_suscripcion_usuario(1).is_ok());
+
+        if let Some(mut usuario) =  plataforma.obtener_usuario(1) {
+            assert!(usuario.suscripcion.is_none());            
+        } else {
+            panic!("Usuario no econtrado");
+        }     
     }
 
     #[test]
     fn testar_plataforma(){
         let mut plataforma = Plataforma::new();
-        plataforma.usuarios.push(crear_usuario(1, Some(suscripcion_basic()), MedioDePago::Efectivo));
-        plataforma.usuarios.push(crear_usuario(2, Some(suscripcion_clasic()), MedioDePago::TarjetaDeCredito { numero: "123".into(), cvv: "123".into() }));
-        plataforma.usuarios.push(crear_usuario(3, Some(suscripcion_super()), MedioDePago::Efectivo));
-        plataforma.usuarios.push(crear_usuario(4, Some(suscripcion_basic()), MedioDePago::Efectivo));
-        plataforma.usuarios.push(crear_usuario(5, Some(suscripcion_basic()), MedioDePago::MercadoPago { cvu: "123".into() }));
+        plataforma.agregar_usuario(1, Some(suscripcion_basic()), MedioDePago::Efectivo);
+        plataforma.agregar_usuario(2, Some(suscripcion_clasic()), MedioDePago::TarjetaDeCredito { numero: "123".into(), cvv: "123".into() });
+        plataforma.agregar_usuario(3, Some(suscripcion_super()), MedioDePago::Efectivo);
+        plataforma.agregar_usuario(4, Some(suscripcion_basic()), MedioDePago::Efectivo);
+        plataforma.agregar_usuario(5, Some(suscripcion_basic()), MedioDePago::MercadoPago { cvu: "123".into() });
 
-        plataforma.usuarios[4].cancelar_suscripcion();
+        if let Some(usuario) = plataforma.obtener_usuario(4) {
+            if let Some(mpago) = plataforma.medio_pago_mas_usado_activo() {
+                assert_eq!(mpago, MedioDePago::Efectivo);
+            }
 
-        if let Some(mpago) = plataforma.medio_pago_mas_usado_activo() {
-            assert_eq!(mpago, MedioDePago::Efectivo);
+            if let Some(tipo) = plataforma.suscripcion_activa_mas_contradada(){
+                assert_eq!(tipo, TipoSuscripcion::Basic);
+            }
+
+            if let Some(mpago) = plataforma.medio_pago_mas_usado_total() {
+                assert_eq!(mpago, MedioDePago::Efectivo);
+            }
+
+            if let Some(tipo) = plataforma.suscripcion_mas_contratada_total(){
+                assert_eq!(tipo, TipoSuscripcion::Basic);
+            }
+            
+        } else {
+            panic!("Usuario no econtrado");
         }
 
-        if let Some(tipo) = plataforma.suscripcion_activa_mas_contradada(){
-            assert_eq!(tipo, TipoSuscripcion::Basic);
-        }
-
-        if let Some(mpago) = plataforma.medio_pago_mas_usado_total() {
-            assert_eq!(mpago, MedioDePago::Efectivo);
-        }
-
-        if let Some(tipo) = plataforma.suscripcion_mas_contratada_total(){
-            assert_eq!(tipo, TipoSuscripcion::Basic);
-        }
+        
 
     }
 
@@ -265,5 +352,25 @@ mod test {
         assert!(plataforma.suscripcion_mas_contratada_total().is_none());
     }
 
+    #[test]
+    fn test_usuario_repetido() {
+        let mut plataforma = Plataforma::new();
+    
+    
+        let resultado1 = plataforma.agregar_usuario(1, Some(suscripcion_basic()), MedioDePago::Efectivo);
+        assert!(resultado1.is_ok());
+    
+    // Segundo usuario con mismo ID debe fallar
+        let resultado2 = plataforma.agregar_usuario(1, Some(suscripcion_clasic()), MedioDePago::TarjetaDeCredito { 
+            numero: "123".into(), 
+            cvv: "123".into() 
+        });
+    
+        if let Err(mensaje) = resultado2 {
+            assert_eq!(mensaje, "Ya existe un usuario con el ID 1");
+        } else {
+            panic!("Se esperaba un error pero el resultado fue Ok");
+        }
+    }
 
 }
